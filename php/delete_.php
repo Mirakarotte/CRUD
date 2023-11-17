c99fcde601ca4d82f731a4b39c707b7fdafba4cb
Mélanie Delaunay <melaniedelaunay816@gmail.com>
2023-11-17 09:02:47 +0100

    Initial commit

---
+++ php/delete_.php
@@ -0,0 +1,29 @@
+<?php 
+session_start();
+
+if (isset($_SESSION['id']) && isset($_SESSION['uname'])) {
+
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DELETE</title>
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+		<form action="delete.php" method="post">
+
+		 	<?php if (isset($_GET['success'])) { ?>
+						<p class="success"><?php echo $_GET['success']; ?></p>
+				<?php } ?>
+					<a href="index.php" class="ca">LOGIN</a>
+		 </form>
+</body>
+</html>
+
+<?php 
+}else{
+		 header("Location: index.php");
+		 exit();
+}
+ ?>
